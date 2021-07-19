a5b31377330194883e202292b152c4ee89f1207e
ApRay <tony19971118@126.com>
2021-07-19 23:47:47 +0800

    1

--- Strassen.cpp
+++ Strassen.cpp
@@ -11,14 +11,15 @@ Strassen<T>::Strassen(int _row_size, int _col_size) {
     this->row_size = _row_size;
     this->col_size = _col_size;
     this->size = _row_size * _col_size;
-    this->matrix = new (T[size]);
+    this->matrix = new T[this->size] ();
+
 }
 
 template <class T>
 Strassen<T> Strassen<T>::operator+(const Strassen<T> &rhs) {
     Strassen<T> res(rhs.row_size, rhs.col_size);
-    for (int i = 0; i < size; i++) {
-        res[i] = this->matrix[i] + rhs.matrix[i];
+    for (int i = 0; i < this->size; i++) {
+        res.matrix[i] = this->matrix[i] + rhs.matrix[i];
     }
     return res;
 }
@@ -27,7 +28,7 @@ template <class T>
 void Strassen<T>::print(const Strassen<T> &s) {
     for (int i = 0; i < s.row_size; i++) {
         for (int j = 0; j < s.col_size; j++) {
-            std::cout << s[i * col_size + j] << "\n";
+            std::cout << s.matrix[i * s.col_size + j] << " ";
         }
         std::cout << "\n";
     }
